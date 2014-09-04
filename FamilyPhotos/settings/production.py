ad1512378f82fe469a2d16412dcd61a8cb4e11d4
salilgupta1 <salil.gupta323@gmail.com>
2014-09-04 00:05:32 -0700

    removing sentry and raven

--- FamilyPhotos/settings/production.py
+++ FamilyPhotos/settings/production.py
@@ -13,12 +13,14 @@ MANAGERS = ADMINS
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
+'''
 RAVEN_CONFIG = {
     'dsn': os.environ['SENTRY_DSN']
 }
+'''
 
 INSTALLED_APPS = INSTALLED_APPS + (
-    'raven.contrib.django.raven_compat',
+#    'raven.contrib.django.raven_compat',
     'lockdown',
 )
 
