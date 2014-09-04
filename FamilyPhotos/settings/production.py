cdd58aa9ae254fe944aec29f39ce58e5915d83b8
salilgupta1 <salil.gupta323@gmail.com>
2014-09-03 23:51:35 -0700

    Project Set Up

---
+++ FamilyPhotos/settings/production.py
@@ -0,0 +1,29 @@
+from common import *
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+ADMINS = (
+    ('Salil Gupta', 'salil.gupta323@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+RAVEN_CONFIG = {
+    'dsn': os.environ['SENTRY_DSN']
+}
+
+INSTALLED_APPS = INSTALLED_APPS + (
+    'raven.contrib.django.raven_compat',
+    'lockdown',
+)
+
+STATIC_ROOT ='staticfiles'
+
+MIDDLEWARE_CLASSES += ('lockdown.middleware.LockdownMiddleware', )
+LOCKDOWN_PASSWORDS = (os.environ['STAGE_PASSWORD'],)
+LOCKDOWN_FORM = 'lockdown.forms.LockdownForm'
