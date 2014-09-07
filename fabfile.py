805060c66f297608ce2991e2ba53dae113976c9d
salilgupta1 <salil.gupta323@gmail.com>
2014-09-07 01:38:24 -0700

    heroku deployments now understand south migrations

--- fabfile.py
+++ fabfile.py
@@ -3,4 +3,5 @@ from fabric.api import local
 def deploy():
 	local('heroku maintenance:on')
 	local('git push heroku master')
+	local('heroku run python manage.py migrate')
 	local('heroku maintenance:off')
