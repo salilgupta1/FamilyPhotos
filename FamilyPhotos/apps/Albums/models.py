47660b4952230ec653c26b375a1778b2d0c5fb9a
salilgupta1 <salil.gupta323@gmail.com>
2014-09-07 03:08:34 -0700

    New album metadata saved in db

--- FamilyPhotos/apps/Albums/models.py
+++ FamilyPhotos/apps/Albums/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Album(models.Model):
 	title = models.CharField(max_length=150)
+	albumUID = models.AutoField(primary_key=True)
 	description = models.CharField(max_length=300,blank=True, null=True)
-	s3Location = models.FilePathField(path="/Albums/")
+	awsS3Storage = models.URLField(max_length=400)
 	timestamp = models.DateField(auto_now_add=True)
--- FamilyPhotos/apps/Albums/views.py
+++ FamilyPhotos/apps/Albums/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from forms import CreateAlbumForm
+from uuid import uuid4
 from django.core.context_processors import csrf
+import os
 
 # Create your views here.
 
@@ -11,8 +13,15 @@ def home(request):
 
 def createAlbum(request):
 	if request.method=="POST":
-		pass
-		#form = CreateAlbumForm(request.POST,request.FILES)
+		form = CreateAlbumForm(request.POST, request.FILES)
+		if form.is_valid():
+			newAlbumForm=form.save(commit=False)
+			S3_BUCKET = os.environ.get("S3_BUCKET")
+			object_name = "Albums/%s" % (str(uuid4()))
+			url ='https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, object_name)
+			tempForm.awsS3Storage = url
+			newAlbumForm.save()
+			#do the actual uploading of the images to amazon s3
 	else:
 		form = CreateAlbumForm()
 	return render(request,"Albums/createAlbum.html",{"form":form})
