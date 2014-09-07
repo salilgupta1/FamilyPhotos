5dba65b53d283a41def593ffe4fb70574bdb24fa
salilgupta1 <salil.gupta323@gmail.com>
2014-09-07 03:07:42 -0700

    migration issues with south

--- FamilyPhotos/apps/Albums/migrations/0002_auto__chg_field_album_awsS3Storage.py
+++ FamilyPhotos/apps/Albums/migrations/0002_auto__chg_field_album_awsS3Storage.py
@@ -8,27 +8,20 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'Album'
-        db.create_table(u'Albums_album', (
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=150)),
-            ('albumUID', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('description', self.gf('django.db.models.fields.CharField')(max_length=300, null=True, blank=True)),
-            ('awsS3Storage', self.gf('django.db.models.fields.URLField')(max_length=200)),
-            ('timestamp', self.gf('django.db.models.fields.DateField')(auto_now_add=True, blank=True)),
-        ))
-        db.send_create_signal(u'Albums', ['Album'])
 
+        # Changing field 'Album.awsS3Storage'
+        db.alter_column(u'Albums_album', 'awsS3Storage', self.gf('django.db.models.fields.URLField')(max_length=400))
 
     def backwards(self, orm):
-        # Deleting model 'Album'
-        db.delete_table(u'Albums_album')
 
+        # Changing field 'Album.awsS3Storage'
+        db.alter_column(u'Albums_album', 'awsS3Storage', self.gf('django.db.models.fields.URLField')(max_length=200))
 
     models = {
         u'Albums.album': {
             'Meta': {'object_name': 'Album'},
             'albumUID': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'awsS3Storage': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'awsS3Storage': ('django.db.models.fields.URLField', [], {'max_length': '400'}),
             'description': ('django.db.models.fields.CharField', [], {'max_length': '300', 'null': 'True', 'blank': 'True'}),
             'timestamp': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '150'})
