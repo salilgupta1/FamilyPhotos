952121c0fbcb8acf0e2f9cd4d94320b24b08cb61
salilgupta1 <salil.gupta323@gmail.com>
2014-11-16 22:06:49 -0800

    more isotope set up

--- FamilyPhotos/apps/Albums/static/Albums/js/FileManager.js
+++ FamilyPhotos/apps/Albums/static/Albums/js/FileManager.js
@@ -1,5 +1,6 @@
 var FileManager = (function($){
-	var setFileInputAttr = function(){
+	var $imageContainer = $("#imagePreview"),
+	setFileInputAttr = function(){
 		// accept multiple file upload
 		$("#id_photos").attr("multiple",true);
 		$("#id_photos").attr("accept","image/*");
@@ -7,21 +8,27 @@ var FileManager = (function($){
 	previewImages = function(){
 		// set up the images for preview before uploading
 		$("#id_photos").change(function(event){
-			$("#imagePreview").empty();
-			// add loading gif here
+			$imageContainer.empty();
 			var input = $(event.currentTarget);
 			var files = input[0].files;
 			var reader;
-			for(var i = 0;i<files.length;i++)
-			{
+			for(var i = 0;i<files.length;i++){
 				reader = new FileReader();
 				reader.onload = function(e){
 					var image = e.target.result;
-					$("#imagePreview").append("<div><img class='photo' src='"+image+"'/></div>");
-					$("#createAlbum").show();
+					$imageContainer.append("<div><img class='photo' src='"+image+"'/></div>");
 				};
 				reader.readAsDataURL(files[i]);
 			}
+		});
+	},
+	createAlbum = function(){
+		$("createAlbum").click(function(e){
+			$("#createAlbumForm").hide();
+			$imageContainer.empty();
+			$imageContainer.append("<img src='{% static 'img/loading.gif' %}'/>");
+			e.preventDefault();
+			e.unbind();
 		});
 	},
 	setUpIsotope = function(container){
@@ -40,6 +47,7 @@ var FileManager = (function($){
 	};
 	return {
 		init:init,
-		setUpIsotope:setUpIsotope
+		setUpIsotope:setUpIsotope,
+		createAlbum:createAlbum,
 	};
 }(jQuery));
